56519c484d00672e451ea0b6d29a2b0d21d45c59
Vikas <vikassandeepkumar@gmail.com>
2022-02-04 14:16:47 +0530

    Updated

--- demo_sliders.py
+++ demo_sliders.py
@@ -13,7 +13,7 @@ class DemoSliders():
         elem2 = driver.find_element_by_xpath("//span[@class='irs-handle to']")
         #ActionChains(driver).drag_and_drop_by_offset(elem1, 60, 0).perform()
         #ActionChains(driver).click_and_hold(elem1).pause(1).move_by_offset(50, 0).release().perform()
-        #ActionChains(driver).move_to_element(elem1).pause(1).click_and_hold(elem1).move_by_offset(80, 0).release().perform()
+        ActionChains(driver).move_to_element(elem1).pause(1).click_and_hold(elem1).move_by_offset(80, 0).release().perform()
         ActionChains(driver).move_to_element(elem2).pause(1).click_and_hold(elem2).move_by_offset(-80, 0).release().perform()
         time.sleep(4)
 
